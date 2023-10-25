0995cedecf6968188bfc946c106292b1553ab72e
Wulan <ASUS@LAPTOP-781R3HVH>
2023-10-25 18:54:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kelima/Data/DataForm.kt
@@ -0,0 +1,5 @@
+package com.example.kelima.Data
+
+data class DataForm(
+    val sex: String = ""
+)
---
+++ app/src/main/java/com/example/kelima/Data/DataSource.kt
@@ -0,0 +1,10 @@
+package com.example.kelima.Data
+
+import com.example.kelima.R
+
+object DataSource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
